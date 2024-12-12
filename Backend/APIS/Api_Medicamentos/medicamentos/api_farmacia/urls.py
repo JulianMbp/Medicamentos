9253f78b829be4f3673dded7d83015928fe1b541
julian <julilito09@gmail.com>
2024-12-12 13:34:56 -0500

    agregar vista de eps con varias card y que se vea bonito

--- Backend/APIS/Api_Medicamentos/medicamentos/api_farmacia/urls.py
+++ Backend/APIS/Api_Medicamentos/medicamentos/api_farmacia/urls.py
@@ -7,9 +7,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('medicamentos/', MedicamentosListView.as_view(), name='medicamentos-list'),
-    path('medicamentos/<int:pk>/', MedicamentosListView.as_view(), name='medicamento-detail'),
-    path('medicamentos/nombre/<str:nombre>/', MedicamentoByNombreView.as_view(), name='medicamento-by-nombre'),
+    path('medicamentos/', MedicamentosListView.as_view(), name='medicamentos-list'), #get y post 
+    path('medicamentos/<int:pk>/', MedicamentosListView.as_view(), name='medicamento-detail'), #put y delete
+    path('medicamentos/nombre/<str:nombre>/', MedicamentoByNombreView.as_view(), name='medicamento-by-nombre'), #get por nombre
     path('blockchain/validate/', BlockchainValidationAPIView.as_view(), name='validate_blockchain'),
     path('blockchain/', BlockchainAPIView.as_view(), name='view_blockchain'),
 ]
