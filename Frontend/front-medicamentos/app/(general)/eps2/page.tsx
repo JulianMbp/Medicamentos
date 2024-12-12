00802f99abf3f4d594d1a14dd9adb487cb690386
julian <julilito09@gmail.com>
2024-12-12 10:23:40 -0500

    agregar vista de eps con varias card y que se vea bonito

--- Frontend/front-medicamentos/app/(general)/eps2/page.tsx
+++ Frontend/front-medicamentos/app/(general)/eps2/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState } from 'react';
 import axios from 'axios';
+import Header from "@/app/sections/header";
 
 interface Medicamento {
   id: number;
@@ -17,9 +18,9 @@ interface Medicamento {
   precio_unitario: number;
 }
 
-export default function Home() {
+export default function Med() {
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchResult, setSearchResult] = useState<Medicamento | null>(null);
+  const [searchResults, setSearchResults] = useState<Medicamento[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
@@ -32,68 +33,83 @@ export default function Home() {
       const response = await axios.get(
         `http://127.0.0.1:8000/api/medicamentos/nombre/${encodeURIComponent(searchTerm)}/`
       );
-      console.log('API Response:', response.data); // Debugging: Verifica la estructura
       const medicamentos = response.data.medicamentos;
 
       if (medicamentos.length > 0) {
-        setSearchResult(medicamentos[0]); // Toma el primer elemento del array
+        setSearchResults(medicamentos);
       } else {
-        setSearchResult(null);
+        setSearchResults([]);
         setError('No se encontraron medicamentos con ese nombre.');
       }
     } catch (err) {
       setError('Hubo un error al cargar los datos.');
       console.error(err);
-      setSearchResult(null);
+      setSearchResults([]);
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <div className="max-w-4xl mx-auto bg-white shadow-md rounded-lg p-6">
-        <h1 className="text-2xl font-bold text-gray-800 mb-4">Buscar Medicamento</h1>
-
-        <div className="flex space-x-2 mb-6">
-          <input
-            type="text"
-            placeholder="Ingrese el nombre del medicamento..."
-            className="flex-1 px-4 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-300"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button
-            onClick={handleSearch}
-            className="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-          >
-            Buscar
-          </button>
+    <main>
+      <Header />
+      <div className="p-6 bg-p-olivine-100 min-h-screen">
+        <h1 className="text-3xl font-bold text-center text-p-olivine-950 mt-6 mb-10">
+          Medicamentos Disponibles
+        </h1>
+        <div className="max-w-xl mx-auto mb-8">
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Ingrese el nombre del medicamento..."
+              className="w-full px-4 py-3 border border-p-olivine-500 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-p-olivine-700 bg-p-olivine-50 text-p-olivine-900 placeholder-p-olivine-500"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button
+              onClick={handleSearch}
+              className="absolute top-1/2 right-2 transform -translate-y-1/2 px-4 py-2 bg-p-olivine-700 text-white rounded-md hover:bg-p-olivine-800 focus:outline-none focus:ring focus:ring-p-olivine-900"
+            >
+              Buscar
+            </button>
+          </div>
         </div>
 
-        {loading && <p className="text-blue-500">Cargando resultado...</p>}
-        {error && <p className="text-red-500">{error}</p>}
+        {loading && <p className="text-p-olivine-700 text-center">Cargando resultados...</p>}
+        {error && <p className="text-red-500 text-center">{error}</p>}
 
-        {searchResult ? (
-          <div className="border p-4 rounded-md shadow-sm bg-gray-50 hover:shadow-md">
-            <h2 className="text-lg font-semibold text-gray-800">{searchResult.nombre}</h2>
-            <p className="text-gray-600">Marca: {searchResult.marca}</p>
-            <p className="text-gray-600">Categoría: {searchResult.categoria}</p>
-            <p className="text-gray-600">Concentración: {searchResult.concentracion} mg</p>
-            <p className="text-gray-600">Existencias: {searchResult.existencias}</p>
-            <p className="text-gray-600">Precio Unitario: ${searchResult.precio_unitario}</p>
-            <p className="text-gray-600">Farmacia: {searchResult.nombreFarmacia}</p>
-            <p className="text-gray-600">Dirección: {searchResult.direccion}</p>
-            <p className="text-gray-600">
-              Requiere Fórmula: {searchResult.formula ? 'Sí' : 'No'}
-            </p>
-            <p className="text-gray-600">Periodicidad: Cada {searchResult.periodicidad} horas</p>
-            <p className="text-gray-600">Cantidad: {searchResult.cantidad}</p>
-          </div>
-        ) : (
-          !loading && !error && <p className="text-gray-600">No hay resultados para mostrar.</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {searchResults.map((medicamento) => (
+            <div
+              key={medicamento.id}
+              className="bg-p-olivine-50 p-6 rounded-lg shadow-lg transition-all duration-300 ease-in-out transform hover:scale-110 hover:border-4 hover:border-p-olivine-800 hover:shadow-2xl hover:shadow-p-olivine-700 hover:bg-p-olivine-200 hover:-translate-y-3"
+            >
+              <h2 className="text-xl font-semibold text-p-olivine-950 mb-2">
+                {medicamento.nombre}
+              </h2>
+              <p className="text-p-olivine-700">Marca: {medicamento.marca}</p>
+              <p className="text-p-olivine-700">Categoría: {medicamento.categoria}</p>
+              <p className="text-p-olivine-700">Concentración: {medicamento.concentracion} mg</p>
+              <p className="text-p-olivine-700">Existencias: {medicamento.existencias}</p>
+              <p className="text-p-olivine-700">Farmacia: {medicamento.nombreFarmacia}</p>
+              <p className="text-p-olivine-700">Dirección: {medicamento.direccion}</p>
+              <p className="text-p-olivine-700">
+                Requiere Fórmula: {medicamento.formula ? 'Sí' : 'No'}
+              </p>
+              <p className="text-p-olivine-700">
+                Periodicidad: Cada {medicamento.periodicidad} horas
+              </p>
+              <p className="text-lg font-bold text-p-olivine-500 hover:text-p-harvest-gold-600 mt-4">
+                ${medicamento.precio_unitario}
+              </p>
+            </div>
+          ))}
+        </div>
+
+        {searchResults.length === 0 && !loading && !error && (
+          <p className="text-p-olivine-700 text-center">No hay resultados para mostrar.</p>
         )}
       </div>
-    </div>
+    </main>
   );
 }
